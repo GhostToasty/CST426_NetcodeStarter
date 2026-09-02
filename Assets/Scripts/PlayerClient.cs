da027fe5abf1294a31ffd3854deaef1e9fb6db99
GhostToast <69281498+GhostToasty@users.noreply.github.com>
2026-09-01 17:25:52 -0700

    video tutorial portion
    - added rpc for proper syncing

--- Assets/Scripts/PlayerClient.cs
+++ Assets/Scripts/PlayerClient.cs
@@ -18,17 +18,48 @@ public class PlayerClient : NetworkBehaviour
         _thirdPersonController.enabled = false;
     }
 
-    //only turns everything back on only for the owner that these properties belong to
+
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
 
+        //only turns everything back on only for the owner that these properties belong to
         if (IsOwner)
         {
             _playerInput.enabled = true;
             _starterAssetsInputs.enabled = true;
+            
+        }
+
+        //third person controller only belongs to the server
+        if (IsServer)
+        {
             _thirdPersonController.enabled = true;
         }
+    }
+
+
+    //sets up RPC
+    //allows client to call function on server, or vice versa
+    //gets client input and runs the logic on the server
+    [Rpc(target:SendTo.Server)]
+    private void UpdateInputServerRpc(UnityEngine.Vector2 move, UnityEngine.Vector2 look, bool jump, bool sprint)
+    {
+        _starterAssetsInputs.MoveInput(move);
+        _starterAssetsInputs.LookInput(look);
+        _starterAssetsInputs.JumpInput(jump);
+        _starterAssetsInputs.SprintInput(sprint);
+    }
+
+    
+    //uses late update to make sure that everything syncs up
+    //player client will call rpc using these values and only processes values on server 
+    private void LateUpdate()
+    {
+        if (!IsOwner)
+            return;
+        
+        UpdateInputServerRpc(_starterAssetsInputs.move, _starterAssetsInputs.look, _starterAssetsInputs.jump, _starterAssetsInputs.sprint);
     }
 
 }
